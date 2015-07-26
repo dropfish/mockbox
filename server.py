d8437c33d4e79c338972409a79e368e18343d317
dropfish <dfish@dropbox.com>
2015-07-25 19:42:36 -0700

    moved index.html so we can serve it on github pages

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory, url_for
+app = Flask(__name__, template_folder='')
 app.debug = True
+
 
 @app.route("/")
 def hello():
