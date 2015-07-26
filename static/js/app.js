8769e173b4d3933aad92373362ff194e8fda08f3
dropfish <dfish@dropbox.com>
2015-07-25 19:32:48 -0700

    first somewhat acceptable version of list view. broke file fetching into two 'servers' a la Dropbox; added styling to list view; updated the states in ui-router

--- static/js/app.js
+++ static/js/app.js
@@ -1,59 +1,145 @@
 var myApp = angular.module('myApp', ['ui.router']);
 
+var DELAY = 500;
+
 myApp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/list");
   $stateProvider
     .state('list', {
       url: "/list",
       resolve: {
-        fileList: function(File) {
-          return File.getList();
+        metadataList: function(Metaserver) {
+          return Metaserver.getList();
         }
       },
       templateUrl: "static/list.html",
-      controller: function($scope, fileList) {
-        $scope.files = fileList;
+      controller: function($scope, metadataList) {
+        $scope.metadataList = metadataList;
+        $scope.sortType = "name";
+        $scope.sortReverse = false;
+        $scope.selectedFile = undefined;
+        $scope.hoveredFile = undefined;
+        $scope.changeSort = function(newSortType) {
+          if (newSortType == $scope.sortType) {
+            // if same sortType applied twice in a row, flip sortReverse
+            $scope.sortReverse = !$scope.sortReverse;
+          }
+          else { // set it to true so that new sortType selections always
+                 // start with sortReverse = false
+            $scope.sortReverse = false;
+          }
+          $scope.sortType = newSortType;
+        };
+        $scope.setSelected = function(newFile) {
+          console.log("setSelected(" + newFile.name + ")");
+          $scope.selectedFile = newFile;
+        };
+        $scope.setHovered = function(newFile) {
+          console.log("setHovered(" + newFile.name + ")");
+          $scope.hoveredFile = newFile;
+        };
+        $scope.resetHovered = function() {
+          $scope.hoveredFile = undefined;
+        }
       }
     })
     .state('view', {
       url: "/view/:filename",
       templateUrl: "static/view.html",
-      controller: function(File, $scope, $stateParams) {
+      params: {
+        test: null,
+        metadata: null
+      },
+      resolve: {
+        metadata: function($stateParams, Metaserver) {
+          console.log("metadata resolve");
+          console.log("RESOLVE stateParams: " + JSON.stringify($stateParams));
+          if ($stateParams.metadata) { // user clicked on ui-sref
+            console.log("already in stateParams");
+            return $stateParams.metadata
+          }
+          else { // user typed URL into address bar, need to fetch metadata
+            console.log("NOT already in stateParams, calling with filename = " + $stateParams.filename);
+            return Metaserver.getByName($stateParams.filename);
+          }
+        }
+      },
+      controller: function($scope, $stateParams, Blockserver, metadata) {
+        var blockserverKey;
         $scope.filename = $stateParams.filename;
-        $scope.file = File.getByName($scope.filename);
+        console.log("stateParams: " + JSON.stringify($stateParams));
+        $scope.metadata = metadata;
+        console.log("metadata: " + JSON.stringify($scope.metadata));
+
+        blockserverKey = $scope.metadata.blockserverKey;
+        console.log("blockserverKey: " + blockserverKey);
+
+        $scope.imagePathOrDocumentContents = Blockserver.getByKey(blockserverKey);
+        console.log("contents: " + JSON.stringify($scope.imagePathOrDocumentContents));
       }
     });
 });
 
-myApp.factory("File", function($http, $timeout, $q) {
-  var defer, fileList, fileToReturn, fileIndex;
+myApp.factory("Metaserver", function($http, $timeout, $q) {
+  console.log("metaserver");
+  var defer, fileList, fileIndex;
   function getList() {
+    console.log("getList (in metaserver)");
     defer = $q.defer()
 
     $timeout(function() {
-      defer.resolve($http.get("static/data/files.json"));
-    }, Math.random()*1000);
+      console.log("timeout completed");
+      defer.resolve($http.get("static/data/metaserver.json"));
+    }, Math.random()*DELAY);
     return defer.promise;
   }
   return {
-    getByName: function(filename) {
+    getByName: function(name) {
+      console.log("getByName(" + name + ")");
       var defer = $q.defer()
       getList().then(function(results) {
         fileList = results.data.files;
-        fileToReturn = undefined;
         for (fileIndex = 0; fileIndex < fileList.length; fileIndex++) {
-          if (fileList[fileIndex].filename == filename) {
+          if (fileList[fileIndex].name == name) {
+            console.log("resolving to " + JSON.stringify(fileList[fileIndex]));
             defer.resolve(fileList[fileIndex]);
+            break;
           }
         }
       });
       return defer.promise;
     },
     getList: function() {
+      console.log("getList()");
       var defer = $q.defer()
       getList().then(function(results) {
         defer.resolve(results.data.files);
       });
+      return defer.promise;
+    }
+  };
+});
+
+myApp.factory("Blockserver", function($http, $timeout, $q) {
+  return {
+    getByKey: function(blockserverKey) {
+      console.log("getByKey(" + blockserverKey + ")");
+      var defer = $q.defer()
+
+      $timeout(function() {
+        $http.get("static/data/blockserver.json").then(function(results) {
+          console.log("results.data: " + JSON.stringify(results.data));
+          if (blockserverKey in results.data) {
+            console.log("blockserverKey in results, it's " + JSON.stringify(results.data[blockserverKey]));
+            defer.resolve(results.data[blockserverKey]);
+          }
+          // implement error checking here and in getByName later
+          // else {
+          //   throw blockserverKey + " not found in blockserver";
+          // }
+        });
+      }, Math.random()*DELAY);
+
       return defer.promise;
     }
   };
